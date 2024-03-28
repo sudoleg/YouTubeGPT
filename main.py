ec1ee5b18ab647c68979d8fe83b118193888217b
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-03-28 10:56:37 +0100

    refactor: main script

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-import re
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
-from modules.youtube import fetch_youtube_transcript, get_video_metadata
-from modules.summary import get_completion_response
-import os
-import json
+from os import getenv
 
-URL: str = "https://youtu.be/tP2sJzeqD34?si=CdpMdLgLsuytlkah"
-    
+from langchain_openai import ChatOpenAI
+
+from modules.helpers import (
+    save_response_as_file,
+)
+
+from modules.summary import get_transcript_summary
+from modules.youtube import (
+    fetch_youtube_transcript,
+    get_video_metadata,
+)
+
+OPENAI_API_KEY = getenv("OPENAI_API_KEY")
+OPENAI_MODEL = getenv("OPEAI_MODEL", "gpt-3.5-turbo")
+TEMPERATURE = float(getenv("MODEL_TEMPERATURE", "0.2"))
+
+
 def main():
+    llm = ChatOpenAI(model_name=OPENAI_MODEL, temperature=TEMPERATURE)
+
     url = input("Enter URL of the YouTube Video: ")
-
     vid_metadata = get_video_metadata(url)
-
-    print(f"We are working with a video called '{vid_metadata['name']}' from {vid_metadata['channel']} on {vid_metadata['provider_name']}.")
-    print("I will summarize the video for you. Hang on...")
+    print(
+        f"We are working with a video called '{vid_metadata['name']}' from {vid_metadata['channel']} on {vid_metadata['provider_name']}."
+    )
+    custom_prompt = input("Enter a custom prompt if you want, otherwise hit 'Enter' :")
     transcript = fetch_youtube_transcript(url)
-    resp = get_completion_response(transcript)
-    
-    # Extract the 'id' value to use as the filename
-    filename = resp['id'] + '.json'
-    
-    # Create the directory if it does not exist
-    directory = './responses'
-    os.makedirs(directory, exist_ok=True)
-    
-    # Construct the full path for the file
-    file_path = os.path.join(directory, filename)
-    
-    # Write the JSON response to a file
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(resp, file, indent=4)
+    if custom_prompt != "":
+        resp = get_transcript_summary(transcript, llm, custom_prompt=custom_prompt)
+    else:
+        resp = get_transcript_summary(transcript, llm)
+
+    print("Summarizing the video for you. Hang on...")
+
+    save_response_as_file(
+        dir_name="./responses", filename=f"{vid_metadata['name']}", file_content=resp
+    )
+    print(resp)
 
 
-main()
+if __name__ == "__main__":
+    main()
