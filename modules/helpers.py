1b4b65f4aa525b3471a4dc38a9ad4aed18693d4b
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-03-28 10:55:03 +0100

    feat: add helpers module

---
+++ modules/helpers.py
@@ -0,0 +1,72 @@
+import re
+import os
+import json
+
+
+def extract_video_id(url):
+    """
+    Extracts the video ID from a given YouTube URL.
+
+    The function supports various YouTube URL formats including standard watch URLs, short URLs, and embed URLs.
+
+    Args:
+        url (str): The YouTube URL from which the video ID is to be extracted.
+
+    Returns:
+        str or None: The extracted video ID as a string if the URL is valid and the video ID is found, otherwise None.
+
+    Example:
+        >>> extract_video_id("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        'dQw4w9WgXcQ'
+        >>> extract_video_id("https://youtu.be/dQw4w9WgXcQ")
+        'dQw4w9WgXcQ'
+        >>> extract_video_id("https://www.youtube.com/embed/dQw4w9WgXcQ")
+        'dQw4w9WgXcQ'
+        >>> extract_video_id("This is not a valid YouTube URL")
+        None
+
+    Note:
+        This function uses regular expressions to match the URL pattern and extract the video ID. It is designed to
+        accommodate most common YouTube URL formats, but may not cover all possible variations.
+    """
+    pattern = r"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})"
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)
+    return None
+
+
+def save_response_as_file(
+    dir_name: str, filename: str, file_content, content_type: str = "text"
+):
+    """
+    Saves given content to a file in the specified directory, formatted as either plain text or JSON.
+
+    Args:
+        dir_name (str): The directory where the file will be saved.
+        filename (str): The name of the file without extension.
+        file_content: The content to be saved. Can be a string for text or a dictionary/list for JSON.
+        content_type (str): The type of content: "text" for plain text or "json" for JSON format. Defaults to "text".
+
+    The function creates the directory if it doesn't exist. It saves `file_content` in a file named `filename`
+    within that directory, adding the appropriate extension (.txt for plain text, .json for JSON) based on `content_type`.
+    For JSON content, it is formatted with an indentation of 4 spaces for readability.
+    """
+    # Create the directory if it does not exist
+    os.makedirs(dir_name, exist_ok=True)
+
+    # Adjust the filename extension based on the content type
+    if content_type == "json":
+        filename += ".json"
+    else:
+        filename += ".txt"
+
+    # Construct the full path for the file
+    file_path = os.path.join(dir_name, filename)
+
+    # Write the content to the file, formatting it according to the content type
+    with open(file_path, "w", encoding="utf-8") as file:
+        if content_type == "json":
+            json.dump(file_content, file, indent=4)
+        else:
+            file.write(file_content)
