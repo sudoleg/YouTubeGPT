4ed6f2867eb9040053bad93a6d38b597cdd27e61
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-03-28 10:55:35 +0100

    refactor: summarize transcript using langchain

--- modules/summary.py
+++ modules/summary.py
@@ -1,44 +1,41 @@
-from requests import api
-import json
-from os import getenv
 from textwrap import dedent
 
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
-API_ENDPOINT = getenv("OPENAI_API_ENDPOINT", "https://api.openai.com")
-OPENAI_API_KEY = getenv("OPENAI_API_KEY")
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant, skilled in summarizing video transcripts, answering questions based on them and providing key take-aways.",
+        ),
+        ("user", "{input}"),
+    ]
+)
 
-req_headers = {
-    "Authorization": f"Bearer {OPENAI_API_KEY}",
-    "Content-Type": "application/json"
-}
+outputparser = StrOutputParser()
 
-def get_completion_response(transcript: str):
-    content = dedent(f"""Summarize the provided video transcript briefly in whole sentences. Here is the transcript, delimited by ---
+
+def get_transcript_summary(transcript: str, llm: ChatOpenAI, **kwargs):
+    user_prompt = dedent(
+        f"""Summarize the provided video transcript briefly in whole sentences. Prefix your response with a short title for the video, consisting of maximum five words and followed by a hyphen. Answer in plain text format.
+                    Here is the transcript, delimited by ---
                     ---
                     {transcript}
                     ---
-                    """)
-    prompt = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant, skilled in summarizing video transcripts and answering questions based on the transcript for general public."
-            },
-            {
-                "role": "user",
-                "content": content
-            }
-        ]
-    }
-
-    response = api.post(
-        url=f"{API_ENDPOINT}/v1/chat/completions",
-        data=json.dumps(prompt),
-        headers=req_headers,
-        timeout=60
+                    """
     )
 
-    resp_text = json.loads(response.text)
-
-    return resp_text
+    if "custom_prompt" in kwargs:
+        user_prompt = dedent(
+            f"""{kwargs['custom_prompt']} 
+                Answer in plain text format.
+                Here is the transcript, delimited by ---
+                ---
+                {transcript}
+                ---
+                """
+        )
+    chain = prompt | llm | outputparser
+    return chain.invoke({"input": user_prompt})
