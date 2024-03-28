dd30cd7a3893421428c9290cf4bc6de96b4aa42d
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-03-28 10:53:56 +0100

    refactor: youtube module
    add new functions and refactoring

--- modules/youtube.py
+++ modules/youtube.py
@@ -1,34 +1,31 @@
-import re
-from youtube_transcript_api import YouTubeTranscriptApi
-from youtube_transcript_api.formatters import TextFormatter
-from requests import api
 import json
 
+from requests import api
+from youtube_transcript_api import YouTubeTranscriptApi, Transcript
+from youtube_transcript_api.formatters import TextFormatter
+
+from .helpers import extract_video_id, save_response_as_file
+
+
 def get_video_metadata(url: str):
-    response = api.get(
-        "https://noembed.com/embed",
-        params={
-            "url": url
-        },
-        timeout=15
-    )
+    response = api.get("https://noembed.com/embed", params={"url": url}, timeout=15)
     json_response = json.loads(response.text)
+    save_response_as_file(
+        dir_name="./video_meta",
+        filename=f"{json_response['title']}",
+        file_content=json_response,
+        content_type="json",
+    )
     return {
-        "name": json_response['title'],
-        "channel": json_response['author_name'],
-        "provider_name": json_response['provider_name']
+        "name": json_response["title"],
+        "channel": json_response["author_name"],
+        "provider_name": json_response["provider_name"],
     }
 
-def fetch_youtube_transcript(url: str):
-    def extract_video_id(url):
-        pattern = r'(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})'
-        match = re.search(pattern, url)
-        if match:
-            return match.group(1)
-        return None
 
+def fetch_youtube_transcript(url: str):
     video_id = extract_video_id(url)
-    if not video_id:
+    if video_id is None:
         return "Error: Could not extract video ID from URL."
 
     try:
@@ -38,3 +35,21 @@ def fetch_youtube_transcript(url: str):
         return transcript
     except Exception as e:
         return f"Error: {str(e)}"
+
+
+def analyze_transcripts(video_id: str):
+    try:
+        transcript_list: list[Transcript] = YouTubeTranscriptApi.list_transcripts(
+            video_id
+        )
+    except Exception as e:
+        print("An error occured when fetching transcripts: " + e)
+        return
+    else:
+        for t in transcript_list:
+            if t.is_generated:
+                print(
+                    f"found auto-generated transcript in {t.language} ({t.language_code})!"
+                )
+            else:
+                print(f"found manual transcript in {t.language} ({t.language_code})!")
