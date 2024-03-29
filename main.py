fade06cf2b7dd3ad0f948162a0a069dc88a1b404
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-03-29 21:07:51 +0100

    feat: add sidebar with app configuration options, resolves #4, ref #12

--- main.py
+++ main.py
@@ -1,41 +1,81 @@
 import json
-from os import getenv
 import logging
+from os import getenv
 
 import streamlit as st
+from langchain_community.callbacks import openai_info
 from langchain_openai import ChatOpenAI
 
 from modules.helpers import save_response_as_file
 from modules.summary import get_transcript_summary
 from modules.youtube import (
+    NoTranscriptFoundException,
     fetch_youtube_transcript,
     get_video_metadata,
-    NoTranscriptFoundException,
 )
 
-from langchain_community.callbacks import openai_info
-
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
-OPENAI_MODEL = getenv("OPENAI_MODEL", "gpt-3.5-turbo")
-TEMPERATURE = float(getenv("MODEL_TEMPERATURE", "0.5"))
+PATH_TO_CONFIG = getenv("CONFIG_PATH", "./config.json")
 
 
-def setup():
-    with open("./config.json", "r", encoding="utf-8") as config_file:
+def get_default_config_value(key_path: str) -> str:
+    """
+    Retrieves a configuration value from a JSON file using a specified key path.
+
+    Args:
+        key_path (str): A string representing the path to the desired value within the nested JSON structure,
+                        with each level separated by a '.' (e.g., "level1.level2.key").
+
+    Returns:
+        The value corresponding to the key path within the configuration file. If the key path does not exist,
+        a KeyError is raised.
+
+    Raises:
+        KeyError: If the specified key path is not found in the configuration.
+    """
+    with open(PATH_TO_CONFIG, "r", encoding="utf-8") as config_file:
         config = json.load(config_file)
 
-    st.set_page_config(
-        page_title=config["app_title"], initial_sidebar_state="collapsed", menu_items={}
-    )
+        keys = key_path.split(".")
+        value = config
+        for key in keys:
+            value = value[key]  # Navigate through each level
+
+        return value
+
+
+def get_available_models() -> tuple[str]:
+    with open(PATH_TO_CONFIG, "r", encoding="utf-8") as config_file:
+        config = json.load(config_file)
+        models = config["available_models"]
+        return tuple(models)
 
 
 def display_error_message(message: str):
     st.error(message)
 
 
+def display_sidebar():
+    with st.sidebar:
+        model = st.selectbox(
+            "Select a model",
+            get_available_models(),
+            key="model",
+            help=get_default_config_value("help_texts.model"),
+        )
+        temperature = st.slider(
+            label="Adjust temperature",
+            value=get_default_config_value("temperature"),
+            min_value=0.0,
+            max_value=2.0,
+            step=0.1,
+            key="temperature",
+            help=get_default_config_value("help_texts.temperature"),
+        )
+
+
 def main():
-    setup()
-    llm = ChatOpenAI(model_name=OPENAI_MODEL, temperature=TEMPERATURE)
+    display_sidebar()
 
     with st.form("form"):
         url = st.text_input("Enter URL of the YouTube Video:")
@@ -51,6 +91,11 @@ def main():
                 )
             try:
                 transcript = fetch_youtube_transcript(url)
+                llm = ChatOpenAI(
+                    api_key=OPENAI_API_KEY,
+                    temperature=st.session_state.temperature,
+                    model=st.session_state.model,
+                )
                 transcript_num_token = llm.get_num_tokens(transcript)
 
                 with st.spinner("Summarizing video. Hang on..."):
@@ -63,7 +108,7 @@ def main():
 
                 st.markdown(resp)
                 st.caption(
-                    f"The transcript has {transcript_num_token} tokens. Estimated cost: {openai_info.get_openai_token_cost_for_model(OPENAI_MODEL, transcript_num_token)}$"
+                    f"The transcript has {transcript_num_token} tokens. Estimated cost: {openai_info.get_openai_token_cost_for_model(model_name=st.session_state.model, num_tokens=transcript_num_token)}$"
                 )
                 save_response_as_file(
                     dir_name="./responses",
