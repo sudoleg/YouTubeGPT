ecd30e5244b9fb7e9eabf3c256d96a8625bf356f
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-11-01 21:55:03 +0100

    feat(library): enable filtering of answers by video

--- pages/library.py
+++ pages/library.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from modules.persistance import SQL_DB, LibraryEntry, delete_library_entry
+from modules.persistance import SQL_DB, LibraryEntry, delete_library_entry, Video
 from modules.ui import display_nav_menu
 
 
@@ -13,11 +13,14 @@ SQL_DB.connect(reuse_if_open=True)
 SQL_DB.create_tables([LibraryEntry], safe=True)
 # --- end ---
 
+tab_summaries, tab_answers = st.tabs(["Summaries", "Answers"])
+
+
+saved_videos = Video.select()
+
+
 saved_lib_entries_summaries = (
     LibraryEntry.select().where(LibraryEntry.entry_type == "S").execute()
-)
-saved_lib_entries_answers = (
-    LibraryEntry.select().where(LibraryEntry.entry_type == "A").execute()
 )
 
 
@@ -27,32 +30,60 @@ def execute_entry_deletion(entry: LibraryEntry):
     st.rerun()
 
 
-if saved_lib_entries_summaries:
-    st.header("Saved summaries")
-    for i, entry in enumerate(saved_lib_entries_summaries, 0):
-        st.caption(f"{entry.video.title} - {entry.video.channel}")
-        with st.expander("Show"):
-            st.write(entry.text)
-        if st.button(
-            label="Delete entry",
-            key=f"delete_summary_{i}",
-        ):
-            execute_entry_deletion(entry)
-        st.divider()
-else:
-    st.info("You don't have any saved summaries yet!")
+with tab_summaries:
+    if saved_lib_entries_summaries:
+        st.header("Saved summaries")
+        for i, entry in enumerate(saved_lib_entries_summaries, 0):
+            st.caption(f"{entry.video.title} - {entry.video.channel}")
+            with st.expander("Show"):
+                st.write(entry.text)
+            if st.button(
+                label="Delete entry",
+                key=f"delete_summary_{i}",
+            ):
+                execute_entry_deletion(entry)
+            st.divider()
+    else:
+        st.info("You don't have any saved summaries yet!")
 
-if saved_lib_entries_answers:
-    st.header("Saved answers")
-    for j, entry in enumerate(saved_lib_entries_answers, 0):
-        st.subheader(entry.question)
-        with st.expander("Show"):
-            st.write(entry.text)
-        if st.button(
-            label="Delete entry",
-            key=f"delete_answer_{j}",
-        ):
-            execute_entry_deletion(entry)
-        st.divider()
-else:
-    st.info("You don't have any saved answers yet!")
+with tab_answers:
+    selected_video_title = st.selectbox(
+        label="Filter by video",
+        placeholder="choose a video or start typing",
+        # only videos with an associated transcript can be selected
+        options=[
+            video.title for video in saved_videos if video.transcripts.count() != 0
+        ],
+        index=None,
+        key="selected_video",
+    )
+
+    if selected_video_title:
+        saved_lib_entries_answers = (
+            LibraryEntry.select()
+            .join(Video)
+            .where(
+                LibraryEntry.entry_type == "A",
+                Video.title == st.session_state.selected_video,
+            )
+            .execute()
+        )
+    else:
+        saved_lib_entries_answers = (
+            LibraryEntry.select().where(LibraryEntry.entry_type == "A").execute()
+        )
+
+    if saved_lib_entries_answers:
+        st.header("Saved answers")
+        for j, entry in enumerate(saved_lib_entries_answers, 0):
+            st.subheader(entry.question)
+            with st.expander("Show"):
+                st.write(entry.text)
+            if st.button(
+                label="Delete entry",
+                key=f"delete_answer_{j}",
+            ):
+                execute_entry_deletion(entry)
+            st.divider()
+    else:
+        st.info("You don't have any saved answers yet!")
